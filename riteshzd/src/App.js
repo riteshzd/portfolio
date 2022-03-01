a481658256ec84013512d1856b350656e0e2a93d
Ritesh Dash <ritesh.dash1@myntra.com>
2022-03-01 22:32:51 +0530

    boiler plate

---
+++ riteshzd/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          We will be online soon
+        </p>
+          Hello world
+      </header>
+    </div>
+  );
+}
+
+export default App;
